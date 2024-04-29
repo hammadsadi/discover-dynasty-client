6d131a7e89d5bafcfeda42abf543d30693eb7d9a
hammadsadi <hammad.sadi@yahoo.com>
2024-04-30 02:21:09 +0600

    Added and Used Lottie react

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,17 +1,20 @@
 import { Link, useRouteError } from "react-router-dom";
-
+import Lottie from "lottie-react";
+import notFound from "../../assets/notfound.json";
 const NotFoundPage = () => {
   const error = useRouteError();
-
+  const customStyle = {
+    width: "300px",
+  };
   return (
     <div className="flex w-full h-screen justify-center items-center flex-col gap-3">
-      <h2 className="text-3xl text-color-secondary md:text-5xl">Sorry</h2>
+      <Lottie animationData={notFound} loop={true} style={customStyle} />
       <h3 className="text-2xl text-color-secondary md:text-3xl">
-        {error.status} {error.statusText}
+        {error.statusText}
       </h3>
       <Link
         to="/"
-        className="btn bg-color-primary text-white font-medium capitalize border-none hover:border-none text-base hover:bg-color-primary "
+        className="  text-black font-medium capitalize border-none hover:border-none text-base hover:text-color-primary hover:underline"
       >
         Back Home
       </Link>
