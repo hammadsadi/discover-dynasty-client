7422eab9edd2141902949928111a6d37b0c4b257
hammadsadi <hammad.sadi@yahoo.com>
2024-07-11 22:33:38 +0600

    Update Product Details Page

--- src/pages/SpotDetails/SpotDetails.jsx
+++ src/pages/SpotDetails/SpotDetails.jsx
@@ -20,7 +20,7 @@ const SpotDetails = () => {
   }, [id]);
 
   return (
-    <div className="container mx-auto px-2 md:px-0">
+    <div className="container mx-auto px-2 md:px-0 mb-5 rounded-md">
       <div className="card card-compact">
         <div className="relative">
           <Fade>
@@ -33,10 +33,10 @@ const SpotDetails = () => {
           </Fade>
         </div>
         <Fade direction="top">
-          <div className="p-2 md:p-6 bg-white ">
+          <div className="p-2 md:p-6 bg-slate-900/30 ">
             <div className="flex md:flex-row flex-col gap-5 md:justify-between md:items-center border-b pb-4">
               <div className="md:space-y-2">
-                <h2 className="text-xl md:text-4xl font-semibold text-color-sd">
+                <h2 className="text-xl md:text-4xl font-semibold text-slate-200">
                   {spot?.touristsSpotName}
                 </h2>
                 <p className="flex gap-2 items-center">
@@ -50,34 +50,39 @@ const SpotDetails = () => {
                     ${spot?.averageCost}
                   </span>
                 </p>
-                <p className="md:text-xl text-sm text-color-secondary font-semibold">
+                <p className="md:text-xl text-sm text-slate-200 font-semibold">
                   Per Person
                 </p>
               </div>
             </div>
             <div className="pt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
               <div className="md:col-span-3">
-                <p className="text-base text-color-opacity">
+                <p className="text-base text-slate-300">
                   {spot?.shortDescription}
                 </p>
                 <div className="flex gap-4 flex-wrap mt-3">
                   <p className="flex gap-2 items-center">
                     <MdOutlineWatchLater className="text-color-primary text-base" />{" "}
-                    <span className="text-base">5 {spot?.travelTime}</span>
+                    <span className="text-base text-slate-300">
+                      {" "}
+                      {spot?.travelTime}
+                    </span>
                   </p>
                   <p className="flex gap-2 items-center">
                     <LuUsers2 className="text-color-primary text-base" />{" "}
-                    <span className="text-base">
+                    <span className="text-base text-slate-300">
                       {spot?.totalVisitorsPerYear}
                     </span>
                   </p>
                   <p className="flex gap-2 items-center">
                     <FaGlobe className="text-color-primary text-base" />{" "}
-                    <span className="text-base">{spot?.countryName}</span>
+                    <span className="text-base text-slate-300">
+                      {spot?.countryName}
+                    </span>
                   </p>
                 </div>
                 <div className="mt-5 border-t pt-4">
-                  <h2 className="text-2xl font-bold text-color-secondary mb-2">
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
                     Included/Exclude
                   </h2>
                   <div className="flex flex-wrap gap-10">
@@ -88,7 +93,7 @@ const SpotDetails = () => {
                           {" "}
                           <p className="flex gap-2 items-center">
                             <FaCheck className="text-color-primary text-base" />{" "}
-                            <span className="text-base">
+                            <span className="text-base text-slate-300">
                               Pick and Drop Services
                             </span>
                           </p>
@@ -97,14 +102,16 @@ const SpotDetails = () => {
                           {" "}
                           <p className="flex gap-2 items-center">
                             <FaCheck className="text-color-primary text-base" />{" "}
-                            <span className="text-base">1 Meal Per Day</span>
+                            <span className="text-base text-slate-300">
+                              1 Meal Per Day
+                            </span>
                           </p>
                         </li>
                         <li>
                           {" "}
                           <p className="flex gap-2 items-center">
                             <FaCheck className="text-color-primary text-base" />{" "}
-                            <span className="text-base">
+                            <span className="text-base text-slate-300">
                               Cruise Dinner & Music Event
                             </span>
                           </p>
@@ -113,7 +120,7 @@ const SpotDetails = () => {
                           {" "}
                           <p className="flex gap-2 items-center">
                             <FaCheck className="text-color-primary text-base" />{" "}
-                            <span className="text-base">
+                            <span className="text-base text-slate-300">
                               Visit 7 Best Places in the City With Group
                             </span>
                           </p>
@@ -127,7 +134,7 @@ const SpotDetails = () => {
                           {" "}
                           <p className="flex gap-2 items-center">
                             <ImCross className="text-color-primary text-base" />{" "}
-                            <span className="text-base">
+                            <span className="text-base text-slate-300">
                               Additional Services
                             </span>
                           </p>
@@ -136,21 +143,27 @@ const SpotDetails = () => {
                           {" "}
                           <p className="flex gap-2 items-center">
                             <ImCross className="text-color-primary text-base" />{" "}
-                            <span className="text-base">Insurance</span>
+                            <span className="text-base text-slate-300">
+                              Insurance
+                            </span>
                           </p>
                         </li>
                         <li>
                           {" "}
                           <p className="flex gap-2 items-center">
                             <ImCross className="text-color-primary text-base" />{" "}
-                            <span className="text-base">Food & Drinks</span>
+                            <span className="text-base text-slate-300">
+                              Food & Drinks
+                            </span>
                           </p>
                         </li>
                         <li>
                           {" "}
                           <p className="flex gap-2 items-center">
                             <ImCross className="text-color-primary text-base" />{" "}
-                            <span className="text-base">Tickets</span>
+                            <span className="text-base text-slate-300">
+                              Tickets
+                            </span>
                           </p>
                         </li>
                       </ul>
@@ -160,15 +173,15 @@ const SpotDetails = () => {
               </div>
               <div className="md:col-span-2">
                 <div className="flex justify-center items-center w-full">
-                  <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-50 dark:text-gray-800 border">
-                    <h1 className="text-2xl font-bold text-center text-color-secondary">
+                  <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-slate-900/20 text-slate-200 border border-slate-900/30">
+                    <h1 className="text-2xl font-bold text-center text-slate-200">
                       Booking Tour
                     </h1>
                     <form className="space-y-6">
                       <div className="space-y-1 text-sm">
                         <label
                           htmlFor="name"
-                          className="block dark:text-gray-600 text-base text-color-sd font-medium"
+                          className="block text-base text-slate-300 font-medium"
                         >
                           Name
                         </label>
@@ -177,13 +190,13 @@ const SpotDetails = () => {
                           name="name"
                           id="name"
                           placeholder="Name"
-                          className="w-full px-4 py-3 rounded-md border text-gray-800 focus:outline-color-primary"
+                          className="w-full px-4 py-3 rounded-md border border-slate-900/20 focus:border-color-primary transition-all duration-300 text-gray-800 focus:outline-none"
                         />
                       </div>
                       <div className="space-y-1 text-sm">
                         <label
                           htmlFor="email"
-                          className="block dark:text-gray-600 text-base text-color-sd font-medium"
+                          className="block text-base text-slate-300 font-medium"
                         >
                           Email
                         </label>
@@ -192,13 +205,13 @@ const SpotDetails = () => {
                           name="email"
                           id="email"
                           placeholder="Email"
-                          className="w-full px-4 py-3 rounded-md border text-gray-800 focus:outline-color-primary"
+                          className="w-full px-4 py-3 rounded-md border border-slate-900/20 focus:border-color-primary transition-all duration-300 text-gray-800 focus:outline-none"
                         />
                       </div>
                       <div className="space-y-1 text-sm">
                         <label
                           htmlFor="number"
-                          className="block dark:text-gray-600 text-base text-color-sd font-medium"
+                          className="block text-base text-slate-300 font-medium"
                         >
                           Number
                         </label>
@@ -207,13 +220,13 @@ const SpotDetails = () => {
                           name="number"
                           id="number"
                           placeholder="Number"
-                          className="w-full px-4 py-3 rounded-md border text-gray-800 focus:outline-color-primary"
+                          className="w-full px-4 py-3 rounded-md border border-slate-900/20 focus:border-color-primary transition-all duration-300 text-gray-800 focus:outline-none"
                         />
                       </div>
                       <div className="space-y-1 text-sm">
                         <label
                           htmlFor="message"
-                          className="block dark:text-gray-600 text-base text-color-sd font-medium"
+                          className="block text-base text-slate-300 font-medium"
                         >
                           Message
                         </label>
@@ -221,7 +234,7 @@ const SpotDetails = () => {
                           name="message"
                           id=""
                           rows="2"
-                          className="w-full px-4 py-3 rounded-md border text-gray-800 focus:outline-color-primary"
+                          className="w-full px-4 py-3 rounded-md border border-slate-900/20 focus:border-color-primary transition-all duration-300 text-gray-800 focus:outline-none"
                         ></textarea>
                       </div>
 
